b5d4ddde159653f6278fe5ce2dc88c7b725805b4
arif <arif-celik56@hotmail.com>
2023-01-11 20:37:05 +0300

    added some error messages

--- backend/src/routers/post.router.js
+++ backend/src/routers/post.router.js
@@ -1,17 +1,8 @@
 const express = require('express');
 const sql = require('../db');
+const queryHandler = require('./queryHandler');
 
 const router = express.Router();
-
-async function queryHandler(query) {
-    try {
-        const data = await query;
-        return [data, null]
-    } catch (err) {
-        console.error('error', err.toString());
-        return [null, err]
-    }
-}
 
 router.get('/', async (req, res) => {
     const [result, error] = await queryHandler(sql` SELECT * FROM posts`)
@@ -23,27 +14,38 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     const { id } = req.params;
-    res.status(200).json({ id: Number(id), body: `post ${id}` })
+    if (!id)
+    return res.status(400).json({ msg: 'error', error: 'id is invalid' })
+    
+    const [result, error] = await queryHandler(sql`SELECT * FROM posts WHERE id = ${id}`)
+    if (error)
+        return res.status(500).json({ msg: 'error', error })
+    if (Object.keys(result).length === 0)
+        return res.status(404).json({ msg: 'error', error: 'post not found' })
+
+    res.status(200).json({ msg: 'success', result: result[0] })
 })
 
 router.post('/add', async (req, res) => {
     const data = req.body;
-    const [result, error] = await queryHandler(sql` INSERT INTO posts ${sql(data, 'title', 'cover', 'body')} RETURNING id `)
+    if (!data || Object.keys(data).length !== 3)
+        return res.status(400).json({ msg: 'error', error: 'invalid arguments' })
+
+    const [result, error] = await queryHandler(sql` INSERT INTO posts ${sql(data, 'title', 'cover', 'body')} RETURNING * `)
     if (error)
         return res.status(500).json({ msg: 'error', error })
 
-    res.status(201).json({ msg: 'success', id: result[0].id });
+    res.status(201).json({ msg: 'success', result: result[0] });
 })
 
 router.patch('/update', async (req, res) => {
     const data = req.body;
     data.updated_at = Date.now();
     const [result, error] = await queryHandler(sql` UPDATE posts SET ${sql(data, 'title', 'cover', 'body', 'updated_at')} WHERE id = ${Number(data.id)} RETURNING * `)
-
     if (error)
         return res.status(500).json({ msg: 'error', error })
 
-    res.status(200).json({ msg: 'updated', result });
+    res.status(200).json({ msg: 'success', result: result[0] });
 })
 
 router.delete('/rm', async (req, res) => {
@@ -52,7 +54,7 @@ router.delete('/rm', async (req, res) => {
     if (error)
         return res.status(500).json({ msg: 'error', error })
 
-    res.status(200).json({ msg: 'deleted', result });
+    res.status(200).json({ msg: 'deleted', result: result[0] });
 })
 
 module.exports = router
